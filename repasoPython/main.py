6fdfbbd6577c7547b29dfe81940eae781511628e
dalzate <diegoalzate.0207@gmail.com>
2023-03-08 19:53:29 -0500

    Se agregan los tipos de datos

---
+++ repasoPython/main.py
@@ -0,0 +1,33 @@
+# Tipos de datos
+# Numero enteros
+# Tipado estático
+numero1: int = 70
+# Tipado dinamico
+numero2 = 45
+print("--------Enteros-----")
+print(numero1)
+print(numero2)
+
+# numeros reales
+# tipado estatico
+numero3: float = 4.9
+
+# tipado dinamico
+numero4 = 5.0
+
+print("--------Reales-----")
+print(numero3)
+print(numero4)
+
+# Booleanos
+esColombiano: bool = True
+esArgentino = False
+
+print("--------Booleanos-----")
+print(esColombiano)
+print(esArgentino)
+
+# Caracter y cadena de caracteres
+print("--------Cadena de caracteres-----")
+mensaje = "cadena con una casilla simple', una comilla doble\", y una diagonal invertida\\"
+print(mensaje)

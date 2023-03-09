bc4a8b06f6ac50b08aa0a5bd470a22afa3a35dc7
Diego Alzate <diegoalzate.0207@gmail.com>
2023-03-08 20:27:10 -0500

    Modificacion con los operadores y relacionales

--- repasoPython/main.py
+++ repasoPython/main.py
@@ -31,3 +31,51 @@ print(esArgentino)
 print("--------Cadena de caracteres-----")
 mensaje = "cadena con una casilla simple', una comilla doble\", y una diagonal invertida\\"
 print(mensaje)
+
+# ----------------------------------------------------------
+# Operadores
+# Aritmeticos
+numero5 = 100
+numero6 = 50
+suma = numero5 + numero6
+resta = numero5 - numero6
+multiplicacion = numero5 * numero6
+division = numero5 / numero6
+modulo = numero5 % numero6
+
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicación es: ", multiplicacion)
+print("La division es: ", division)
+print("El modulo es: ", modulo)
+
+# Asignacion
+x = 5
+y = 7
+z = 3
+
+# Lógicos
+# AND = Y
+print("---------Lógicos AND-------------")
+p = 5
+print(p > 4 and p < 9)
+print("---------Lógicos OR-------------")
+p = 5
+print(p < 7 or p < 9)
+print("---------Lógicos NOT-------------")
+p = 5
+print(not(p > 2 and p < 7))
+print("---------------------------------")
+
+# Relacionales o de comparacion
+edad1 = 23
+edad2 = 18
+
+print(edad1 == edad2) # igualdad
+print(edad1 > edad2) # mayor que
+print(edad1 < edad2) # menor que
+print(edad1 >= edad2) #mayor igual que
+print(edad1 <= edad2) #menor igual que
+print(edad1 != edad2) #diferente o no igual
+
+
